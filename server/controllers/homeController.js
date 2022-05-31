8e3895f558db99d28807d698915289fdd6dec108
EmmanuelAVT9 <alexsoge@gmail.com>
2022-05-30 21:26:03 -0500

    S36 About Route

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -1,30 +1,41 @@
 // URL: Get /
 const index = (req, res) => {
-    // Calculando emojie
-    const emojieDataset = [
-      '💻',
-      '👨‍💻',
-      '🎈',
-      '🎄',
-      '🦺',
-      '🎢',
-      '🎁',
-      '🚆',
-      '🌍',
-      '♥',
-    ];
-    const emojie =
-      emojieDataset[Math.floor(Math.random() * emojieDataset.length)];
-    // View-Models
-    const viewModel = {
-      title: 'Index Controller Working!!!',
-      author: 'Emmanuel Vázquez',
-      emojie,
-    };
-    res.render('index', viewModel);
+  // Calculando emojie
+  const emojieDataset = [
+    '💻',
+    '👨‍💻',
+    '🎈',
+    '🎄',
+    '🦺',
+    '🎢',
+    '🎁',
+    '🚆',
+    '🌍',
+    '♥',
+  ];
+  const emojie =
+    emojieDataset[Math.floor(Math.random() * emojieDataset.length)];
+  // View-Models
+  const viewModel = {
+    title: 'Index Controller Working!!!',
+    author: 'Emmanuel Vázquez',
+    emojie,
+  };
+  res.render('index', viewModel);
 };
-  
+
+// URL: Get /about
+const about = (req, res) => {
+  res.render('home/aboutView', {
+    // Este es el View Model
+    name: 'Emmanuel Vázquez',
+    email: 'prueba@gmail.com',
+    url: 'www.itgam.com/emmanuelVazquez',
+  });
+};
+
 export default {
-    // Action Methods
-    index,
+  // Action Methods
+  index,
+  about,
 };
